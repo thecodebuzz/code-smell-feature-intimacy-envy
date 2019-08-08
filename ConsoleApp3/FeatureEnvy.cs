4989207acbbe937f937e44f6083a34b42c96c518
CodeBuzz <codebuzz.net@gmail.com>
2019-08-08 00:00:58 -0400

    updated

---
+++ ConsoleApp3/FeatureEnvy.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class FeatureEnvy
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Bad
+    {
+        class Item
+        {
+            public bool IsOutOfStock;
+            public decimal Price;
+            public decimal Tax;
+            public bool IsOnSale;
+            public decimal SaleDiscount;
+        }
+
+        class Basket
+        {
+            decimal GetTotalPrice(Item i)
+            {
+                //Item class instances is being used multiple time in another instance of class
+                //There are total 4 count of occurr instances 
+                if (i.IsOutOfStock) //1 
+                    throw new Exception("Item ${i} is out of stock.");
+                var price = i.Price + i.Tax;//2
+                if (i.IsOnSale)//3
+                    price = price - i.SaleDiscount * price;//4
+                return price;
+            }
+        }
+    }
+
+    class Good
+    {
+        class Item
+        {
+            public bool IsOutOfStock;
+            public decimal Price;
+            public decimal Tax;
+            public bool IsOnSale;
+            public decimal SaleDiscount;
+
+            decimal GetTotalPrice(Item i)
+            {
+                if (i.IsOutOfStock)
+                    throw new Exception("Item ${i} is out of stock.");
+                var price = i.Price + i.Tax;
+                if (i.IsOnSale)
+                    price = price - i.SaleDiscount * price;
+                return price;
+            }
+        }
+    }
+
+
+
+}
+namespace FeatureEnvySmell
+{
+
+    class ContactInfo
+    {
+        public string GetStreetName()
+        {
+            return "1 Tower Square HartFord";
+        }
+        public string GetCity()
+        {
+            return "HartFord";
+        }
+        public string GetState()
+        {
+            return "CT";
+        }
+    }
+
+    class User
+    {
+        private ContactInfo _contactInfo;
+        User(ContactInfo contactInfo)
+        {
+            _contactInfo = contactInfo;
+        }
+        public string GetFullAddress(ContactInfo info)
+        {
+            string city = info.GetCity();//1
+            string state = info.GetState();//2
+            string streetName = info.GetStreetName();//3
+            return streetName + ";" + city + ";" + state;
+        }
+
+    }
+
+}
+
+namespace FeatureEnvySmellResolved
+{
+
+    class ContactInfo
+    {
+        public string GetStreetName()
+        {
+            return "1 Tower Square HartFord";
+        }
+        public string GetCity()
+        {
+            return "HartFord";
+        }
+        public string GetState()
+        {
+            return "CT";
+        }
+        public string GetFullAddress(ContactInfo info)
+        {
+            string city = info.GetCity();//1
+            string state = info.GetState();//2
+            string streetName = info.GetStreetName();//3
+            return streetName + ";" + city + ";" + state;
+        }
+    }
+
+    class User
+    {
+        private ContactInfo _contactInfo;
+        User(ContactInfo contactInfo)
+        {
+            _contactInfo = contactInfo;
+        }
+    }
+
+}
---
+++ ConsoleApp3/InappropriateIntimacy-Bi-Directional.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace WithCodeSmell
+{
+    class A
+    {
+        private B _b;
+    }
+
+    class B
+    {
+        private A _a;
+    }
+}
+
+namespace FixedCodeSmellApporach1
+{
+    class A
+    {
+        private B _b;
+    }
+    class B
+    {
+
+    }
+}
+namespace FixedCodeSmellApporach2
+{
+    class A
+    {
+        private B _b;
+
+        A(IClassB classb)
+        {
+            _b = (B)classb;
+        }
+    }
+
+    interface IClassB
+    {
+    }
+
+    class B:IClassB
+    {
+
+    }
+}
